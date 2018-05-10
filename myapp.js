e87066f046d06c1b3d05cb7b80f554e9f7b0e111
gpajaresm <gpm3@hotmail.es>
2018-05-10 23:50:49 +0200

    Añadidos titulos peliculas

--- myapp.js
+++ myapp.js
@@ -120,12 +120,15 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
+  var y = document.getElementsByClassName("btn2");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+     x[i].style.display = "none";
+     y[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex-1].style.display = "block"; 
+  y[slideIndex-1].style.display = "block";
 }
 
 
